321b98087adb1e44065834f2393e5ef3572f92ce
xy6au <56407106+xy6au@users.noreply.github.com>
2019-11-21 14:31:52 -0500

    initial

---
+++ map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken='pk.eyJ1IjoieHk2YXUiLCJhIjoiY2sxdXlwejk5MGZ5aTNncXV3MGx0Y3JmcCJ9.2LvX99rC6vL08hPV1pVhxg';
+var map=new mapboxgl.Map({
+container:'map',
+style:'mapbox://styles/xy6au/ck2o2xm3o0cce1cnmyic93upe'
+
+});
